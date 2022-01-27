cb1f01592c61aa401a8f9e332684e05f11ed2825
LimaOscarData <leventogut01@gmail.com>
2022-01-27 20:15:04 +0300

    small changes for working on the file

--- Apply_method_pandas.py
+++ Apply_method_pandas.py
@@ -2,7 +2,7 @@ import  pandas as pd
 import numpy as np
 
 train= pd.read_csv('http://bit.ly/kaggletrain')
-print(train.head())
+# print(train.head())
 #  PassengerId  Survived  Pclass  ...     Fare Cabin  Embarked
 # 0            1         0       3  ...   7.2500   NaN         S
 # 1            2         1       1  ...  71.2833   C85         C
@@ -57,6 +57,7 @@ def get_element(my_list, position):
 
 # you can't use the function's double brackets inside the apply method .
 # So you can add after comma , (position) . Not inside the function.
+# print(train['Name'].str.split(","))
 # print(train['Name'].str.split(",").apply(get_element, position=0).head())
 # 0       Braund
 # 1      Cumings
@@ -96,7 +97,7 @@ print(drinks.head())
 # The axis means below is the direction for the function.
 # If axis is 0 : to the right wise, 1 is for downstream.
 a=drinks.loc[:, 'beer_servings': 'wine_servings'].apply(max, axis=0)
-# # print(a)
+print(a)
 # [5 rows x 6 columns]
 # beer_servings      376
 # spirit_servings    438
@@ -117,7 +118,7 @@ b = drinks.loc[:, 'beer_servings':'wine_servings'].apply(max, axis=1).head()
 #----------------------------------------------------------
 
 c = drinks.loc[:, 'beer_servings':'wine_servings'].apply(np.argmax, axis=1).head()
-print(c)
+# print(c)
 # 0    0
 # 1    1
 # 2    0
@@ -130,9 +131,7 @@ print(c)
 # APPLY MAP :
 #  It is a dataframe method
 # Apply a function to every element in a dataframe.
-
-drinks.loc[:, 'beer_servings': 'wine_servings'] = \
-drinks.loc[:, 'beer_servings': 'wine_servings'].applymap(float)
+drinks.loc[:, 'beer_servings': 'wine_servings'] = drinks.loc[:, 'beer_servings': 'wine_servings'].applymap(float)
 print(drinks.head())
 
 #        country  beer_servings  ...  total_litres_of_pure_alcohol  continent
